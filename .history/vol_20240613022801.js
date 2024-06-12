7320ea297513b9ebdcba1569acb21383656b5b18
Le Minh Duyet <duyetleminh2004@gmail.com>
2024-06-13 03:08:23 +0700

    lmd

---
+++ .history/vol_20240613022801.js
@@ -0,0 +1,26 @@
+let audioElement = new Audio();
+
+let vol_icon = document.getElementById("vol_icon");
+let vol = document.getElementById("vol");
+let vol_dot = document.getElementById("vol_dot");
+let vol_bar = document.getElementsByClassName("vol_bar")[0];
+
+vol.addEventListener("change", () => {
+  let vol_a = vol.value;
+  vol_bar.style.width = `${vol_a}%`;
+  vol_dot.style.left = `${vol_a}%`;
+  music.volume = vol_a / 100;
+  if (vol_a == 0) {
+    vol_icon.classList.remove("bi-volume-down-fill");
+    vol_icon.classList.add("bi-volume-mute-fill");
+    vol_icon.classList.remove("bi-volume-up-fill");
+  } else if (vol_a > 0 && vol_a <= 50) {
+    vol_icon.classList.remove("bi-volume-mute-fill");
+    vol_icon.classList.remove("bi-volume-up-fill");
+    vol_icon.classList.add("bi-volume-down-fill");
+  } else {
+    vol_icon.classList.remove("bi-volume-mute-fill");
+    vol_icon.classList.remove("bi-volume-down-fill");
+    vol_icon.classList.add("bi-volume-up-fill");
+  }
+});
